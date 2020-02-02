b37bbabbef67653ab0a3db495b950137a3b4cb46
Ayush Sharma <ayushsharma.22@gmail.com>
2020-02-02 21:56:08 +0530

    Adding exception handling and functions to read and write files.

--- helpers.py
+++ helpers.py
@@ -11,19 +11,19 @@ def _config(key):
     key {string} -- Name of the key in the config.json file.
     """
 
-    my_file = Path("config.json")
-    if not my_file.is_file():
+    my_file = _read_file("config.json")
+    if not my_file:
         print("--- Main config.json file not found. Exiting.")
         sys.exit()
 
-    config_file = open("config.json")
-    config = config_file.read()
+    try:
 
-    if not config:
+        config = json.loads(my_file)
+
+    except:
+
         print("--- config.json invalid. Exiting.")
         sys.exit()
-
-    config = json.loads(config)
 
     if config.get(key):
         return config.get(key)
@@ -48,3 +48,53 @@ def _error(message):
     message {string} -- Log message.
     """
     print(f" _error > {message}")
+
+
+def _read_file(path):
+    """ Read file if it exists, and False on error.
+
+    Arguments:
+    path {string} -- Path to file.
+    """
+
+    file = Path(path)
+    if not file.is_file():
+
+        return False
+
+    try:
+
+        file = open(path)
+        data = file.read()
+        file.close()
+    
+    except Exception as e:
+
+        _error("Exception reading file.")
+        _error(e)
+
+    return data
+
+
+def _write_file(path, data):
+    """ Write data to file, overwriting existing file if it exists. Return False on error.
+
+    Arguments:
+    path {string} -- Path to file.
+    data {string} -- Content to write.
+    """
+
+    try:
+
+        file = open(path, mode="w")
+        file.write(data)
+        file.close()
+
+    except Exception as e:
+
+        _error('Exception writing file.')
+        _error(e)
+
+        return False
+
+    return True
