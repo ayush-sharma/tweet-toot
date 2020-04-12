cea4b1f02d41953762cd5f1f1d44e4c2089dcb03
Ayush Sharma <ayushsharma.22@gmail.com>
2020-04-12 19:19:38 +0530

    Adding #!/usr/bin/env python3 to all Python files.

--- tweet-toot/run.py
+++ tweet-toot/run.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import encodings.idna
 import helpers
 import social
--- tweet-toot/social.py
+++ tweet-toot/social.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import helpers
 import requests
 from bs4 import BeautifulSoup
