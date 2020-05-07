fab438c9eba02f3ddb3665aa3e2b02ca93d31c59
Ayush Sharma <ayush-sharma@users.noreply.github.com>
2020-05-08 05:08:46 +0530

    Removing "new"

--- tweet-toot/tweettoot.py
+++ tweet-toot/tweettoot.py
@@ -129,7 +129,7 @@ class TweetToot:
             return False
 
         logger.info(
-            f"get_tweets() => Fetched {len(timeline)} new tweets for {self.twitter_url}."
+            f"get_tweets() => Fetched {len(timeline)} tweets for {self.twitter_url}."
         )
 
         for tweet in timeline:
